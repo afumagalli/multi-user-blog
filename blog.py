8210df2a960073035e34cd6244622c48d8250be5
Anthony Fumagalli <anthony.fumagalli@gmail.com>
2016-05-18 09:25:33 -0700

    Some refactoring

--- blog.py
+++ blog.py
@@ -1,4 +1,10 @@
 import os
+import re
+import codecs
+import hashlib
+import hmac
+import random
+import string
 import webapp2
 import jinja2
 
@@ -7,12 +13,7 @@ from google.appengine.ext import db
 def blog_key(name = 'default'):
     return db.Key.from_path('blogs', name)
 
-class Post(db.Model):
+class BlogPost(db.Model):
     subject = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
-
-    def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", p = self)
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os
+import re
+import codecs
+import hashlib
+import hmac
+import random
+import string
 import webapp2
 import jinja2
 
@@ -13,7 +19,6 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
 class Handler(webapp2.RequestHandler):
-    """Defines functions used by all other handlers"""
     def write(self, *a, **kw):
         self.response.write(*a, **kw)
 
@@ -24,32 +29,126 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-    def set_secure_cookie(self, name, val):
-        cookie_val = make_secure_val(val)
-        self.response.headers.add_header(
-            'Set-Cookie',
-            '%s=%s; Path=/' % (name, cookie_val))
-
-    def read_secure_cookie(self, name):
-        cookie_val = self.request.cookies.get(name)
-        return cookie_val and check_secure_val(cookie_val)
-
-    def login(self, user):
-        self.set_secure_cookie('user_id', str(user.key().id()))
-
-    def logout(self):
-        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
-
-    def initialize(self, *a, **kw):
-        webapp2.RequestHandler.initialize(self, *a, **kw)
-        uid = self.read_secure_cookie('user_id')
-        self.user = uid and User.by_id(int(uid))
-
 class MainHandler(Handler):
-    """Renders the homepage"""
     def get(self):
         self.render("index.html")
 
+class Rot13Handler(Handler):
+    def get(self):
+        self.render("rot13.html")
+
+    def post(self):
+        text = self.request.get("text")
+        if text:
+            text = codecs.encode(text, 'rot_13')
+        self.render('rot13.html', text = text)
+
+class SignupHandler(Handler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        user_error = False
+        pwd_error = False
+        verify_error = False
+        email_error = False
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+
+        if not username or not valid_username(username):
+            user_error = True
+        if not password or not verify or not valid_password(password):
+            pwd_error = True
+        if password != verify:
+            verify_error = True
+        if email and not valid_email(email):
+            email_error = True
+
+        if user_error or pwd_error or verify_error or email_error:
+            self.render("signup.html", user_error = user_error,
+                                       pwd_error = pwd_error,
+                                       verify_error = verify_error,
+                                       email_error = email_error,
+                                       username = username,
+                                       email = email)
+        else:
+            user = User(username = username, pwd_hash = make_pw_hash(username, password), email = email)
+            user.put()
+            user_cookie = make_secure_val(str(username))
+            self.response.headers.add_header("Set-Cookie", "user=%s; Path=/" % user_cookie)
+            self.redirect("/welcome")
+
+class WelcomeHandler(Handler):
+    def get(self):
+        user = self.request.cookies.get('user')
+        if user:
+            username = check_secure_val(user)
+            if username:
+                self.render("welcome.html", username = username)
+            else:
+                self.redirect('/signup')
+        else:
+            self.redirect('/signup')
+
+class LoginHandler(Handler):
+    def get(self):
+        self.render("login.html")
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        user = db.GqlQuery("SELECT * FROM User WHERE username = '%s'" % username)[0]
+        if valid_pw(username, password, user.pwd_hash):
+            user_cookie = make_secure_val(str(username))
+            self.response.headers.add_header("Set-Cookie", "user=%s; Path=/" % user_cookie)
+            self.redirect("/welcome")
+        else:
+            self.render("login.html")
+
+class LogoutHandler(Handler):
+    def get(self):
+        self.response.headers.add_header("Set-Cookie", "user=; Path=/")
+        self.redirect("/signup")
+
+class BlogHandler(Handler):
+    def get(self):
+        posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC")
+        self.render("blog.html", posts = posts)
+
+class NewPostHandler(Handler):
+    def get(self):
+        self.render("newpost.html")
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+        if subject and content:
+            post = BlogPost(parent = blog_key(), subject = subject, content = content)
+            post.put()
+            self.redirect('/blog/%s' % str(post.key().id()))
+        else:
+            error = "you need both a subject and content"
+            self.render("newpost.html", subject = subject, content = content, error = error)
+
+class PostPage(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('BlogPost', int(post_id), parent=blog_key())
+        print "Key = " + str(key)
+        post = db.get(key)
+        print post
+        if not post:
+            self.error(404)
+            return
+        self.render("blogpost.html", post = post)
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/rot13', Rot13Handler),
+    ('/signup', SignupHandler),
+    ('/welcome', WelcomeHandler),
+    ('/login', LoginHandler),
+    ('/logout', LogoutHandler),
+    ('/blog', BlogHandler),
+    ('/blog/newpost', NewPostHandler),
+    ('/blog/([0-9]+)', PostPage)
 ], debug=True)
