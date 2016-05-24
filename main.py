7d81235d49078bb02342712025112e862c3101d4
Anthony Fumagalli <anthony.fumagalli@gmail.com>
2016-05-23 17:47:12 -0700

    Implemented janky but working like button

--- main.py
+++ main.py
@@ -40,12 +40,6 @@ class Handler(webapp2.RequestHandler):
         cookie_val = self.request.cookies.get(name)
         return cookie_val and check_secure_val(cookie_val)
 
-    # def login(self, user):
-    #     self.set_secure_cookie('user_id', str(user.key().id()))
-
-    # def logout(self):
-    #     self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
-
     def initialize(self, *a, **kw):
         webapp2.RequestHandler.initialize(self, *a, **kw)
         username = self.read_secure_cookie('user')
@@ -108,6 +102,7 @@ class SignupHandler(Handler):
                 user.put()
                 user_cookie = make_secure_val(str(username))
                 self.response.headers.add_header("Set-Cookie", "user=%s; Path=/" % user_cookie)
+                time.sleep(0.1)
                 self.redirect("/welcome")
 
 class WelcomeHandler(Handler):
@@ -173,21 +168,42 @@ class PostHandler(Handler):
         key = ndb.Key('BlogPost', int(post_id), parent=blog_key())
         post = key.get()
         comments = Comment.gql("WHERE post_id = %s ORDER BY created DESC" % int(post_id))
+        liked = None
+        if self.user:
+            liked = Like.gql("WHERE post_id = :1 AND author.username = :2", int(post_id), self.user.username).get()
         if not post:
             self.error(404)
             return
-        self.render("blogpost.html", post = post, comments = comments)
+        self.render("blogpost.html", post = post, comments = comments, liked = liked)
     def post(self, post_id):
         key = ndb.Key('BlogPost', int(post_id), parent=blog_key())
         post = key.get()
-        content = self.request.get("content")
-        if content:
-            comment = Comment(content = str(content), author = self.user, post_id = int(post_id))
-            comment.put()
-            time.sleep(0.1)
+        if self.request.get("like"):
+            if post and self.user:
+                post.likes += 1
+                like = Like(post_id = int(post_id), author = self.user)
+                like.put()
+                post.put()
+                time.sleep(0.2)
+            self.redirect("/blog/%s" % post_id)
+        elif self.request.get("unlike"):
+            if post and self.user:
+                post.likes -= 1
+                like = Like.gql("WHERE post_id = :1 AND author.username = :2", int(post_id), self.user.username).get()
+                key = like.key
+                key.delete()
+                post.put()
+                time.sleep(0.2)
             self.redirect("/blog/%s" % post_id)
         else:
-            self.render("blogpost.html", post = post)
+            content = self.request.get("content")
+            if content:
+                comment = Comment(content = str(content), author = self.user, post_id = int(post_id))
+                comment.put()
+                time.sleep(0.1)
+                self.redirect("/blog/%s" % post_id)
+            else:
+                self.render("blogpost.html", post = post)
 
 class EditPostHandler(Handler):
     def get(self):
