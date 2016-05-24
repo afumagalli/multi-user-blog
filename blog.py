c71631dee8ee9d13387453340d561be073b6f05d
Anthony Fumagalli <anthony.fumagalli@gmail.com>
2016-05-23 17:58:33 -0700

    Commenting

--- blog.py
+++ blog.py
@@ -13,9 +13,11 @@ from users import *
 from google.appengine.ext import ndb
 
 def blog_key(name = 'default'):
+    """Assigns a key to BlogPost"""
     return ndb.Key('blogs', name)
 
 class BlogPost(ndb.Model):
+    """Contains info about a blog post"""
     subject = ndb.StringProperty(required = True)
     content = ndb.TextProperty(required = True)
     created = ndb.DateTimeProperty(auto_now_add = True)
@@ -23,11 +25,13 @@ class BlogPost(ndb.Model):
     likes = ndb.IntegerProperty(default = 0)
 
 class Comment(ndb.Model):
+    """Contains info about a comment"""
     post_id = ndb.IntegerProperty(required = True)
     author = ndb.StructuredProperty(User)
     content = ndb.StringProperty(required = True)
     created = ndb.DateTimeProperty(auto_now_add = True)
 
 class Like(ndb.Model):
+    """Contains info about a like"""
     post_id = ndb.IntegerProperty(required = True)
     author = ndb.StructuredProperty(User)
--- main.py
+++ main.py
@@ -19,37 +19,46 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
 class Handler(webapp2.RequestHandler):
+    """Defines functions for rendering pages and setting cookies"""
     def write(self, *a, **kw):
+        """Writes to the web page"""
         self.response.write(*a, **kw)
 
     def render_str(self, template, **kw):
+        """Renders a Jinja template"""
         kw['user'] = self.user
         t = jinja_env.get_template(template)
         return t.render(kw)
 
     def render(self, template, **kw):
+        """Writes rendered template to page"""
         self.write(self.render_str(template, **kw))
 
     def set_secure_cookie(self, name, val):
+        """Sets a cookie""""
         cookie_val = make_secure_val(val)
         self.response.headers.add_header(
             'Set-Cookie',
             '%s=%s; Path=/' % (name, cookie_val))
 
     def read_secure_cookie(self, name):
+        """Reads a cookie and returns its value"""
         cookie_val = self.request.cookies.get(name)
         return cookie_val and check_secure_val(cookie_val)
 
     def initialize(self, *a, **kw):
+        """Initializes the page with the signed-in user"""
         webapp2.RequestHandler.initialize(self, *a, **kw)
         username = self.read_secure_cookie('user')
         self.user = User.gql("WHERE username = '%s'" % username).get()
 
 class MainHandler(Handler):
+    """Renders the front page"""
     def get(self):
         self.render("index.html")
 
 class Rot13Handler(Handler):
+    """Handles the secret Rot13 page"""
     def get(self):
         self.render("rot13.html")
 
@@ -60,6 +69,7 @@ class Rot13Handler(Handler):
         self.render('rot13.html', text = text)
 
 class SignupHandler(Handler):
+    """Handles the signup page"""
     def get(self):
         self.render("signup.html")
 
@@ -76,19 +86,19 @@ class SignupHandler(Handler):
 
         user = User.gql("WHERE username = '%s'" % username).get()
         if user:
-            exist_error = True
+            exist_error = True # user already exists
             self.render("signup.html", exist_error = exist_error,
                                        username = username,
                                        email = email)
         else:
             if not username or not valid_username(username):
-                user_error = True
+                user_error = True # username invalid
             if not password or not verify or not valid_password(password):
-                pwd_error = True
+                pwd_error = True # password invalid
             if password != verify:
-                verify_error = True
+                verify_error = True # passwords don't match
             if email and not valid_email(email):
-                email_error = True
+                email_error = True # email invalid
 
             if user_error or pwd_error or verify_error or email_error:
                 self.render("signup.html", user_error = user_error,
@@ -98,6 +108,7 @@ class SignupHandler(Handler):
                                            username = username,
                                            email = email)
             else:
+                # Everything is good, register the user
                 user = User(username = username, pwd_hash = make_pw_hash(username, password), email = email)
                 user.put()
                 user_cookie = make_secure_val(str(username))
@@ -106,6 +117,7 @@ class SignupHandler(Handler):
                 self.redirect("/welcome")
 
 class WelcomeHandler(Handler):
+    """Renders the welcome page"""
     def get(self):
         user = self.request.cookies.get('user')
         if user:
@@ -118,6 +130,7 @@ class WelcomeHandler(Handler):
             self.redirect('/signup')
 
 class LoginHandler(Handler):
+    """Handles user login"""
     def get(self):
         self.render("login.html")
 
@@ -134,16 +147,19 @@ class LoginHandler(Handler):
             self.render("login.html", username = username, error = error)
 
 class LogoutHandler(Handler):
+    """Handles user logout, redirects to signup on completion"""
     def get(self):
         self.response.headers.add_header("Set-Cookie", "user=; Path=/")
         self.redirect("/signup")
 
 class BlogHandler(Handler):
+    """Renders the main blog page"""
     def get(self):
         posts = BlogPost.gql("ORDER BY created DESC")
         self.render("blog.html", posts = posts)
 
 class NewPostHandler(Handler):
+    """Handles creation of new posts"""
     def get(self):
         if self.user:
             self.render("newpost.html")
@@ -152,6 +168,7 @@ class NewPostHandler(Handler):
 
     def post(self):
         if not self.user:
+            # if no user, redirect to blog page
             self.redirect("/blog")
         subject = self.request.get("subject")
         content = self.request.get("content")
@@ -164,6 +181,7 @@ class NewPostHandler(Handler):
             self.render("newpost.html", subject = subject, content = content, error = error)
 
 class PostHandler(Handler):
+    """Renders the page for a single post, handles comments and likes on post"""
     def get(self, post_id):
         key = ndb.Key('BlogPost', int(post_id), parent=blog_key())
         post = key.get()
@@ -179,6 +197,7 @@ class PostHandler(Handler):
         key = ndb.Key('BlogPost', int(post_id), parent=blog_key())
         post = key.get()
         if self.request.get("like"):
+            # User liked post
             if post and self.user:
                 post.likes += 1
                 like = Like(post_id = int(post_id), author = self.user)
@@ -187,6 +206,7 @@ class PostHandler(Handler):
                 time.sleep(0.2)
             self.redirect("/blog/%s" % post_id)
         elif self.request.get("unlike"):
+            # User unliked post
             if post and self.user:
                 post.likes -= 1
                 like = Like.gql("WHERE post_id = :1 AND author.username = :2", int(post_id), self.user.username).get()
@@ -196,6 +216,7 @@ class PostHandler(Handler):
                 time.sleep(0.2)
             self.redirect("/blog/%s" % post_id)
         else:
+            # User commented on post
             content = self.request.get("content")
             if content:
                 comment = Comment(content = str(content), author = self.user, post_id = int(post_id))
@@ -206,6 +227,7 @@ class PostHandler(Handler):
                 self.render("blogpost.html", post = post)
 
 class EditPostHandler(Handler):
+    """Handles editing of blog posts"""
     def get(self):
         if self.user:
             post_id = self.request.get("post")
@@ -238,6 +260,7 @@ class EditPostHandler(Handler):
             self.redirect("/blog")
 
 class DeletePostHandler(Handler):
+    """Handles deletion of blog posts"""
     def get(self):
         if self.user:
             post_id = self.request.get("post")
@@ -260,6 +283,7 @@ class DeletePostHandler(Handler):
         self.redirect("/blog")
 
 class EditCommentHandler(Handler):
+    """Handles editing of comments"""
     def get(self):
         if self.user:
             comment_id = self.request.get("comment")
@@ -290,6 +314,7 @@ class EditCommentHandler(Handler):
             self.redirect("/blog/%s" % comment.post_id)
 
 class DeleteCommentHandler(Handler):
+    """Handles deletion of comments"""
     def get(self):
         if self.user:
             comment_id = self.request.get("comment")
